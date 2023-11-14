efca3bd96f87c3665680e2775e2f3d95fc38b5ca
Ahmed Raza <110291380+AhmedRaza02@users.noreply.github.com>
2023-11-14 04:43:23 +0000

    nums and maths

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive Data Types
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions.
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// *********************************************
+
+// Stack (Primitive), Heap (Non Primitive)
